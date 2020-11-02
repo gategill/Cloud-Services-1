4abca8520cba0ac0eb530a53caada45fa29cf880
Misiaczek <michal.p.szarkowicz@gmail.com>
2020-11-02 15:53:41 +0000

    fixed simulator

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -3,6 +3,8 @@ import os
 import argparse
 import numpy as np
 import shutil
+import subprocess
+
 
 class Argument:
     def __init__(self):
@@ -16,7 +18,7 @@ class Argument:
         parser = argparse.ArgumentParser(description = "Simulator Configurations")
 
         parser.add_argument("-ni", "--set_iterations", nargs = "+", required = True, default = 1, type = int,      
-                            help = "Set number and sizes of iterations, required, eg -ni 1 5 10")
+                            help = "Set number and sizes of iterations, required, e.g., -ni 1 5 10")
     
         return parser.parse_args()
         
@@ -25,13 +27,12 @@ class Argument:
         self.iterations = self.parsed_args.set_iterations
 
 
-
 class Simulator:
-
     argument = Argument()
 
+
     def __init__(self):
-        self.run_simulation()
+        self.run_simulator()
        
         
     def get_time_folder(self):
@@ -39,7 +40,7 @@ class Simulator:
     
     
     def get_time_file(self, program, num_iterations):
-        file_name =  "{}__{}_iterations.txt".format(program, num_iterations)
+        file_name = "{}__{}_iterations.txt".format(program, num_iterations)
         file_path = os.path.join(self.get_time_folder(), file_name)
         return file_path
 
@@ -50,23 +51,30 @@ class Simulator:
             
         os.mkdir(self.get_time_folder())
             
-            
-    def get_programs(self):
-        program_folder = os.path.join(os.getcwd(), "programs")
-        programs = os.listdir(program_folder)
-        return programs
-
-
-    def run_simulation(self):
+    
+    def get_program_names(self):
+        programs_folder = os.path.join(os.getcwd(), "programs")
+        program_names = os.listdir(programs_folder)
+        return program_names
+    
+                
+    def get_program_path(self, program_name):
+        programs_folder = os.path.join(os.getcwd(), "programs")
+        program_path = os.path.join(programs_folder, program_name) 
+        return program_path
+    
+    
+    def run_simulator(self):
         self.create_folder()
 
-        for program in self.get_programs():
-            time_list = []
-            
-            for num_iterations in list(self.argument.iterations):
+        for program_name in self.get_program_names():
+            for num_iterations in list(self.argument.iterations): # let know
+                time_list = []
+                
                 for iteration in range(num_iterations):
                     tic = time.time()
-                    os.system(program) 
+                    program_path = self.get_program_path(program_name)
+                    subprocess.call(["python", program_path])
                     toc = time.time()
                     
                     time_passed = toc - tic
@@ -75,7 +83,7 @@ class Simulator:
                 mean = np.mean(time_list)
                 std = np.std(time_list)
                 
-                with open(self.get_time_file(program, num_iterations), "a") as time_txt:
+                with open(self.get_time_file(program_name, num_iterations), "a") as time_txt:
                     time_txt.write(str(mean) + "\n")
                     time_txt.write(str(std) + "\n\n")
                     
