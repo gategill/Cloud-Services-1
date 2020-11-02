54d90c19438eba751bbf07d45ac8ed4e368cb1bb
Misiaczek <michal.p.szarkowicz@gmail.com>
2020-11-02 14:08:52 +0000

    improved simulator

---
+++ simulator/programs/hello_world.py
@@ -0,0 +1,15 @@
+class Hello_World:
+    def __init__(self):
+        self.do_it()
+        
+        
+    def do_it(self):
+        print("hello world")
+        
+def main():
+    Hello_World()
+    
+if __name__ == "__main__":
+    main()
+    
+        
